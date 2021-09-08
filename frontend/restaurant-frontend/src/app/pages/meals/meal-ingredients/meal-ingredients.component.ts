b6623717fefdcbc5c18fb1195b536d1c2489073c
Weronika Rynkowska <weronar@interia.pl>
2021-09-08 16:50:31 +0200

    Fix bugs

--- frontend/restaurant-frontend/src/app/pages/meals/meal-ingredients/meal-ingredients.component.ts
+++ frontend/restaurant-frontend/src/app/pages/meals/meal-ingredients/meal-ingredients.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Ingredient, IngredientResponse } from '../../../model/RestaurantModel';
 
 @Component({
   selector: 'app-meal-ingredients',
   templateUrl: './meal-ingredients.component.html',
-  styleUrls: ['./meal-ingredients.component.less']
+  styleUrls: ['./meal-ingredients.component.less'],
 })
 export class MealIngredientsComponent implements OnInit {
+  tableDataSource!: MatTableDataSource<Ingredient>;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openDialog() {
+    console.log("Open");
   }
 
+  onNoClick() {
+    console.log("Open");
+  }
+
+  addIngredient() {
+    console.log('Add ingredient');
+  }
+
+  deleteIngredient(ingredient: Ingredient) {
+    console.log('Delete ingredient');
+  }
 }
